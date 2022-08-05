0a92fd40355c053b2c13d239f6de8c77aa4461e2
JediJake66 <jacobstout59@gmail.com>
2022-08-05 09:27:01 -0400

    From added

---
+++ custreg/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import CustReg from './components/CustReg'
+import logo from './logo.svg';
+
+function App() {
+  return (
+    
+    <div className="App">    
+      <CustReg />
+    </div>
+  );
+}
+
+export default App;
